f2fe8d281995b8959108b20525d9dd894e2da2b0
BerkAkipek <berk.akipek.96@gmail.com>
2022-06-01 15:29:08 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer;
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+    globalThis.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer (){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'karen'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'ali';
+}
+
